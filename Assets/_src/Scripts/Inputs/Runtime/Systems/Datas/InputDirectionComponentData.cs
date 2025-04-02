506d18aa47161f26f2cf9ab570c2d3e11296c0dc
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-04-02 21:43:44 +0600

    Organized file properly

---
+++ Assets/_src/Scripts/Inputs/Runtime/Systems/Datas/InputDirectionComponentData.cs
@@ -0,0 +1,12 @@
+﻿#if !BL_DISABLE_INPUT
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace BovineLabs.Core.Input
+{
+    public partial struct InputDirectionComponentData : IComponentData
+    {
+        [InputActionDown] public float3 Value;
+    }
+}
+#endif
---
+++ Assets/_src/Scripts/Movements/Runtime/Datas/TargetPositionComponentData.cs
@@ -0,0 +1,10 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace _src.Scripts.Movements.Runtime.Datas
+{
+    public struct TargetPositionComponentData : IComponentData
+    {
+        public float3 Value;
+    }
+}
