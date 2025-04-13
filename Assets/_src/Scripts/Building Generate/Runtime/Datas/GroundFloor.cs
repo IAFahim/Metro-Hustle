d81a727b6012a7565c3e8b26a11a0808de083f99
Bushra Hossain <bushrahossainonika@gmail.com>
2025-04-13 16:06:16 +0600

    Array of rooftop middlefloor and groundfloor

---
+++ Assets/_src/Scripts/Building Generate/Runtime/Datas/GroundFloor.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+namespace _src.Scripts.Building_Generate.Runtime.Datas
+{
+    public struct GroundFloor : IBufferElementData
+    {
+        public float GroundFloorEntity;
+    }
+}
