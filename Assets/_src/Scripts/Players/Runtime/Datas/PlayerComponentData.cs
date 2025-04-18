a9308046877c8dc49a8b480e968abc87c1231067
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-04-18 13:18:25 +0600

    Adding cinemachine lookup

---
+++ Assets/_src/Scripts/Players/Runtime/Datas/PlayerComponentData.cs
@@ -0,0 +1,8 @@
+using Unity.Entities;
+
+namespace _src.Scripts.Players.Runtime.Datas
+{
+    public struct PlayerComponentData : IComponentData
+    {
+    }
+}
---
+++ Assets/_src/Scripts/Players/Runtime/Datas/PlayerComponentDataAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace _src.Scripts.Players.Runtime.Datas
+{
+    public class PlayerComponentDataAuthoring : MonoBehaviour
+    {
+        public class PlayerComponentDataBaker : Baker<PlayerComponentDataAuthoring>
+        {
+            public override void Bake(PlayerComponentDataAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.Dynamic);
+                AddComponent<PlayerComponentData>(entity);
+            }
+        }
+    }
+}
