e3122262bae8db8280f2bea71e01a33116356828
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2025-04-23 16:51:19 +0600

    should build

--- Assets/_src/Scripts/Speeds/Runtime/Datas/SpeedComponentDataAuthoring.cs
+++ Assets/_src/Scripts/Speeds/Runtime/Datas/SpeedComponentDataAuthoring.cs
@@ -1,28 +1,21 @@
 ﻿#if UNITY_EDITOR
-using System;
-using System.Linq;
 using BovineLabs.Core.Authoring.ObjectManagement;
-using BovineLabs.Core.Authoring.Settings;
-using JetBrains.Annotations;
 using Unity.Entities;
 using UnityEngine;
 
 namespace _src.Scripts.Speeds.Runtime.Datas
 {
     [RequireComponent(typeof(ObjectDefinitionAuthoring))]
-    public class SpeedComponentDataAuthoring : LookupAuthoring<SpeedRegistry, float>
+    public class SpeedComponentDataAuthoring : MonoBehaviour
     {
-        public float speed = 1;
         public float multiplier = 1f;
-        [NonSerialized] [CanBeNull] private ObjectDefinitionAuthoring _objectDefinition;
-        [NonSerialized] [CanBeNull] private SpeedSettings _speedSettings;
+        
 
         public class SpeedComponentDataBaker : Baker<SpeedComponentDataAuthoring>
         {
             public override void Bake(SpeedComponentDataAuthoring authoring)
             {
-                if (authoring._speedSettings == null && !authoring.TryGetInitialization(out float speed)) return;
-                DependsOn(authoring._speedSettings);
+                
                 var entity = GetEntity(TransformUsageFlags.Dynamic);
                 AddComponent(entity, new SpeedMultiplierComponentData
                 {
@@ -30,24 +23,7 @@ namespace _src.Scripts.Speeds.Runtime.Datas
                 });
             }
         }
-
-        private void OnValidate()
-        {
-            _objectDefinition = GetComponent<ObjectDefinitionAuthoring>();
-            TryGetInitialization(out var _);
-        }
-
-
-        public override bool TryGetInitialization(out float value)
-        {
-            value = speed;
-            if (_speedSettings == null) _speedSettings = AuthoringSettingsUtility.GetSettings<SpeedSettings>();
-            if (_speedSettings == null) return false;
-            if (_objectDefinition == null) return false;
-            if (_objectDefinition.Definition == null) return false;
-            _speedSettings.Set(_objectDefinition.Definition.ID, value);
-            return true;
-        }
+        
     }
 }
 #endif
